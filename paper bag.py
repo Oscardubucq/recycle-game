ff0b1ae12cd50334f5214ad4272e3185be3ae391
Oscardubucq <oscar.dubucq@gmail.com>
2025-01-03 16:57:39 +0100

    Update paper bag.py

--- paper bag.py
+++ paper bag.py
@@ -23,15 +23,15 @@ def draw ():
     else :
         for item in items:
             item.draw()
-
+            
 def update():
     global items
     if len(items)==0 :
         items = make_items(current_level)
-
+        
 def make_items(number_of_extra_items):
     items_to_create = get_option_to_create(number_of_extra_items)
-
+    
     new_items = create_items(items_to_create)
     layout_items(new_items)
     animate_items(new_items)
@@ -42,9 +42,34 @@ def get_option_to_create (number_of_extra_items):
         random_option = random.choice(ITEMS)
         items_to_create.append(random_option)
     return items_to_create
+    
+def create_items (items_to_create):
+    new_items = []
+    for option in items_to_create:
+        item = Actor(option + "img")
+        new_items.append(item)
+    return new_items
+    
+def layout_items(items_to_layout):
+    number_of_gaps = len(items_to_layout) + 1
+    gap_size = WIDTH / number_of_gaps
+    random.shuffle(items_to_layout)
+    
+    for index,item in enumerate(items_to_layout):
+        new_x_pos = (index + 1) * gap_size
+        item.x = new_x_pos
 
+def animate_items(items_to_animate):
+    global animations 
+    for item in items_to_animate:
+        duration = start_speed - current_level
+        item.anchor = ("center","bottom")
+        animation = animate(item,duration = duration,on_finished = handle_game_over,y = HEIGHT)
+        animations.append(animation)
+        
+def handle_game_over():
+    global game_over
+    game_over = True
+    
 
-
-
-
-
+   
